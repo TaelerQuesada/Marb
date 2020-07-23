578a296f85ab32153aedfc2350735f07525d7bc9
TaelerQuesada <taelerquesada@gmail.com>
2020-07-23 16:06:30 -0700

    Initial Commit
    The first commit.

---
+++ Assets/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.Animations;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Transform follow;
+    public float turnSpeed = 5f;
+    public float camDistance = 10f;
+
+    private Vector3 offset;
+    private float currentX = 0f;
+    private float currentY = 0f;
+
+    public float sensitivityX = 5f;
+    public float sensitivityY = 5f;
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentX += Input.GetAxis("Mouse X");
+        currentY += Input.GetAxis("Mouse Y");
+    }
+    void LateUpdate()
+    {
+
+        Vector3 direction = new Vector3(0f, 0f, -camDistance);
+        Quaternion rotation = Quaternion.Euler(currentY * sensitivityY, currentX * sensitivityX, 0f);
+        transform.position = follow.position + rotation * direction;
+        transform.LookAt(follow.position);
+    }
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    Rigidbody rb;
+    public GameObject follow;
+    public Transform cam;
+    public float speed = 6f;
+    public float jump = 7f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+
+        Transform hold = cam;
+        Vector3 eulerRotation = hold.rotation.eulerAngles;
+        hold.rotation = Quaternion.Euler(0f, eulerRotation.y, eulerRotation.z);
+        Vector3 camFront = hold.forward;
+
+        
+        Vector3 moveRelativeVertical = camFront * Input.GetAxis("Vertical") 
+            + hold.right * Input.GetAxis("Horizontal");
+ 
+        rb.AddForce(moveRelativeVertical * speed);
+
+        follow.transform.position = transform.position;
+
+    }
+}
